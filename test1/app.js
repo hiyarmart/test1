7b449af066674c720b5bb3eb94fd8ba04d602354
hiyarmart <146836137+hiyarmart@users.noreply.github.com>
2024-12-16 17:46:50 +0600

    Add files via upload

---
+++ test1/app.js
@@ -0,0 +1,30 @@
+const regExp = /Регулярные выражения/
+
+const containsOnlyDigits = (str) => {
+    return // Ваш код
+}
+
+onsole.log(containsOnlyDigits("12345")); // Выведет true
+console.log(containsOnlyDigits("12a45")); // Выведет false
+
+
+
+ console.log("Прошла секунда"); // 1
+ console.log("Прошла секунда"); // 2
+ console.log("Прошла секунда"); // 3
+
+
+
+const count = () => {
+    let i = 1;
+    const interval = setInterval( // Правильно продолжите... );
+
+}
+
+{
+  "key1": "value",
+  "key2": "value",
+  "key3": "value"
+}
+
+
